fe48b32cbecf22cbf9a63f7b32922a7ade738844
Lucas G Accurcio Ferreira <lucas.ferreira@brisabr.com.br>
2023-07-22 18:31:05 -0300

    feat: function to store and add participant with useState

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,26 +1,31 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Alert, FlatList, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
-import { styles } from './styles';
 import { Participant } from './components/Participant';
+import { styles } from './styles';
 
 export function Home() {
-  const participants = [
-    'Lucas Accurcio',
-    'Felippe Accurcio',
-    'Tiago Accurcio',
-    'Anna Smith',
-    'John Doe',
-    'Jane Doe',
-    'John Smith',
-    'Jane Smith',
-    'John Wayne',
-    'Jane Wayne'
-  ]
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participant, setParticipant] = useState('');
+  // const participants = [
+  //   'Lucas Accurcio',
+  //   'Felippe Accurcio',
+  //   'Tiago Accurcio',
+  //   'Anna Smith',
+  //   'John Doe',
+  //   'Jane Doe',
+  //   'John Smith',
+  //   'Jane Smith',
+  //   'John Wayne',
+  //   'Jane Wayne'
+  // ]
 
   function handleParticipantAdd(): void {
-    if (participants.includes('Lucas Accurcio')) {
-      return Alert.alert('Você já está participando do evento');
+    if (participants.includes(participant)) {
+      return Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome');
     }
+    setParticipants(prevState => [...prevState, participant]);
+    setParticipant('');
   }
 
   function handleParticipantRemove(name: string): void {
@@ -44,8 +49,10 @@ export function Home() {
       <View style={styles.form}>
         <TextInput
           style={styles.input}
-          placeholder="Digite seu nome"
+          placeholder="Nome do participante"
           placeholderTextColor={'#808080'}
+          onChangeText={setParticipant}
+          value={participant}
         />
         <StatusBar style="auto" />
 
